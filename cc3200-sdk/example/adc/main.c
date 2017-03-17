9e8270062dffa5257a0a5154cf01ac0f144a2045
Siarhei Volkau <lis82@mail.by>
2017-03-17 17:23:38 +0300

    ADC ex.: make/cmake build scripts, gcc related fixes

--- cc3200-sdk/example/adc/main.c
+++ cc3200-sdk/example/adc/main.c
@@ -92,7 +92,7 @@ unsigned long pulAdcSamples[4096];
 //*****************************************************************************
 //                      GLOBAL VARIABLES
 //*****************************************************************************
-#if defined(ccs)
+#if defined(gcc) || defined(ccs)
 extern void (* const g_pfnVectors[])(void);
 #endif
 #if defined(ewarm)
@@ -141,7 +141,7 @@ BoardInit(void)
     //
     // Set vector table base
     //
-#if defined(ccs)
+#if defined(gcc) || defined(ccs)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif
 #if defined(ewarm)
@@ -167,7 +167,7 @@ BoardInit(void)
 //! \return none
 //!
 //*****************************************************************************
-void 
+int
 main()
 {
     unsigned long  uiAdcInputPin;  
@@ -235,7 +235,7 @@ main()
                 uiChannel = ADC_CH_3;
                 break;
             default:
-                break;
+                continue;
         }
 
         //
