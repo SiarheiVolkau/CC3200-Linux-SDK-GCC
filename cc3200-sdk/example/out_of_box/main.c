07ca5375267aeb78cd1887ebcbf8899547dc616d
Siarhei Volkau <lis8215@gmail.com>
2017-03-18 18:48:15 +0300

    Out of Box ex.: make/cmake build scripts, gcc related fixes

--- cc3200-sdk/example/out_of_box/main.c
+++ cc3200-sdk/example/out_of_box/main.c
@@ -134,7 +134,7 @@ static unsigned char  g_ucConnectionSSID[SSID_LEN_MAX+1]; //Connection SSID
 static unsigned char  g_ucConnectionBSSID[BSSID_LEN_MAX]; //Connection BSSID
 
 
-#if defined(ccs)
+#if defined(ccs) || defined(gcc)
 extern void (* const g_pfnVectors[])(void);
 #endif
 #if defined(ewarm)
@@ -1091,7 +1091,7 @@ BoardInit(void)
     //
     // Set vector table base
     //
-#if defined(ccs)
+#if defined(ccs) || defined(gcc)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif  //ccs
 #if defined(ewarm)
@@ -1112,7 +1112,7 @@ BoardInit(void)
 //****************************************************************************
 //                            MAIN FUNCTION
 //****************************************************************************
-void main()
+int main()
 {
     long   lRetVal = -1;
 
