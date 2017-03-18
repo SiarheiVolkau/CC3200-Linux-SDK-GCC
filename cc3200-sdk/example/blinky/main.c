a51b33e7e8fbb8a2173973540e43996c837d437a
Siarhei Volkau <lis8215@gmail.com>
2017-03-18 08:56:06 +0300

    blinky ex.: make/cmake build scripts, gcc related fixes

--- cc3200-sdk/example/blinky/main.c
+++ cc3200-sdk/example/blinky/main.c
@@ -82,7 +82,7 @@
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
 //*****************************************************************************
-#if defined(ccs)
+#if defined(gcc) || defined(ccs)
 extern void (* const g_pfnVectors[])(void);
 #endif
 #if defined(ewarm)
@@ -162,7 +162,7 @@ BoardInit(void)
     //
     // Set vector table base
     //
-#if defined(ccs)
+#if defined(gcc) || defined(ccs)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif
 #if defined(ewarm)
