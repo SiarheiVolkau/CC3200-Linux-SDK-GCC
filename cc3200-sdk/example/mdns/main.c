4c4363d73af7891d34c77fc7745ce8b5bd379b81
Siarhei Volkau <lis8215@gmail.com>
2017-03-18 16:23:36 +0300

    MDNS ex.: make/cmake build scripts, gcc related fixes

--- cc3200-sdk/example/mdns/main.c
+++ cc3200-sdk/example/mdns/main.c
@@ -530,11 +530,11 @@ static long WlanConnect()
     SlSecParams_t secParams = {0};
     long lRetVal = 0;
 
-    secParams.Key = SECURITY_KEY;
+    secParams.Key = (signed char*)SECURITY_KEY;
     secParams.KeyLen = strlen(SECURITY_KEY);
     secParams.Type = SECURITY_TYPE;
 
-    lRetVal = sl_WlanConnect(SSID_NAME, strlen(SSID_NAME), 0, &secParams, 0);
+    lRetVal = sl_WlanConnect((signed char*)SSID_NAME, strlen(SSID_NAME), 0, &secParams, 0);
     ASSERT_ON_ERROR(lRetVal);
 
     // Wait for WLAN Event
@@ -563,7 +563,7 @@ BoardInit(void)
     //
     // Set vector table base
     //
-#if defined(ccs)
+#if defined(ccs) || defined(gcc)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif
 #if defined(ewarm)
@@ -679,10 +679,10 @@ static long mDNS_Task()
       while(iretvalmDNS)
       {
         ulTextLen = 200;
-        iretvalmDNS = sl_NetAppDnsGetHostByService(SERVICE_NAME,(unsigned char)\
-                          strlen(SERVICE_NAME),SL_AF_INET,\
-                          (unsigned long *)&pAddr,\
-                          &usPort,&ulTextLen,&cText[0]);
+        iretvalmDNS = sl_NetAppDnsGetHostByService((signed char*)SERVICE_NAME,
+                          (unsigned char)strlen(SERVICE_NAME),SL_AF_INET,
+                          (unsigned long *)&pAddr,
+                          &usPort,&ulTextLen,(signed char*)&cText[0]);
       }
       if(iretvalmDNS == 0)
       {
