335b84ee76c5f954cc6beaba2cefbe4e0f0b16b0
Siarhei Volkau <lis8215@gmail.com>
2017-03-19 09:50:07 +0300

    Sleep ex.: make/cmake build scripts, gcc related fixes

--- cc3200-sdk/example/sleep/main.c
+++ cc3200-sdk/example/sleep/main.c
@@ -91,7 +91,7 @@
 //*****************************************************************************
 //                 GLOBAL VARIABLES -- Start
 //*****************************************************************************
-#if defined(ccs)
+#if defined(ccs) || defined(gcc)
 extern void (* const g_pfnVectors[])(void);
 #endif
 #if defined(ewarm)
@@ -292,7 +292,7 @@ BoardInit(void)
   //
   // Set vector table base
   //
-#if defined(ccs)
+#if defined(ccs) || defined(gcc)
     MAP_IntVTableBaseSet((unsigned long)&g_pfnVectors[0]);
 #endif
 #if defined(ewarm)
@@ -325,7 +325,7 @@ BoardInit(void)
 //! \return None.
 //
 //****************************************************************************
-void main()
+int main()
 {
     //
     // Initialize the MCU
