4aa120c11a7fafdac6e3604c7253f92429f88a78
Siarhei Volkau <lis8215@gmail.com>
2017-09-01 08:33:57 +0300

    changes for growee project

--- cc3200-sdk/third_party/FreeRTOS/source/FreeRTOSConfig.h
+++ cc3200-sdk/third_party/FreeRTOS/source/FreeRTOSConfig.h
@@ -110,7 +110,7 @@
 #define configUSE_TIMERS				1
 #define configTIMER_TASK_PRIORITY		5
 #define configTIMER_QUEUE_LENGTH		20
-#define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
+#define configTIMER_TASK_STACK_DEPTH	( 1024 )
 
 /* Debug */
 #define configQUEUE_REGISTRY_SIZE				10
